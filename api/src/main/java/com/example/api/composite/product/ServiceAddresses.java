32791e4f7a6ba3f37ea95183f7c717cfbf8fdbac
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-06-24 05:00:15 +0200

    define product-composite service api

---
+++ api/src/main/java/com/example/api/composite/product/ServiceAddresses.java
@@ -0,0 +1,36 @@
+package com.example.api.composite.product;
+
+public class ServiceAddresses {
+    private final String cmp, pro, rev, rec;
+
+    public ServiceAddresses() {
+        this.cmp = null;
+        this.pro = null;
+        this.rev = null;
+        this.rec = null;
+    }
+
+    public ServiceAddresses(String compositeAddress, String productAddress,
+                            String reviewAddress, String recommendationAddress) {
+        this.cmp = compositeAddress;
+        this.pro = productAddress;
+        this.rev = reviewAddress;
+        this.rec = recommendationAddress;
+    }
+
+    public String getCmp() {
+        return cmp;
+    }
+
+    public String getPro() {
+        return pro;
+    }
+
+    public String getRev() {
+        return rev;
+    }
+
+    public String getRec() {
+        return rec;
+    }
+}
