18e627d7ffdd89b43dd6ae00eca834f8c5785e20
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-07-03 15:05:12 +0200

    non-block REST api in review-service

--- api/src/main/java/com/example/api/core/review/ReviewService.java
+++ api/src/main/java/com/example/api/core/review/ReviewService.java
@@ -3,6 +3,8 @@ package com.example.api.core.review;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface ReviewService {
   @PostMapping(
@@ -10,12 +12,12 @@ public interface ReviewService {
           consumes = "application/json",
           produces = "application/json"
   )
-  Review createReview(@RequestBody Review body);
+  Mono<Review> createReview(@RequestBody Review body);
 
   @GetMapping(value = "/reviews", produces = "application/json")
-  List<Review> getReviews(@RequestParam(value = "productId", required = true) int productId);
+  Flux<Review> getReviews(@RequestParam(value = "productId", required = true) int productId);
 
   @DeleteMapping(value = "/reviews")
-  void deleteReviews(@RequestParam(value = "productId", required = true)
+  Mono<Void> deleteReviews(@RequestParam(value = "productId", required = true)
                      int productId);
 }
